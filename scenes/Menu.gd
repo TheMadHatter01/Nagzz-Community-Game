500464362127e178c934c36ea0dece1d2a422f78
Zoaz <108300982+ZoazVA@users.noreply.github.com>
2022-07-09 03:56:11 +0300

    Add main menu (#13)
    * added main menu
    
    * main menu
    
    * main menu

---
+++ scenes/Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	pass 
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://scenes/test_scene.tscn")
+
+func _on_Options_pressed():
+	pass 
+
+func _on_Credits_pressed():
+	pass 
+
+func _on_Quit_pressed():
+	get_tree().quit()
