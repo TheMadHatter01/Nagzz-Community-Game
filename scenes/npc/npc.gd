1b9ca4e215283ba194f472a29bc0ba78be0113f2
echooff2 <23408914+echooff2@users.noreply.github.com>
2022-08-07 19:04:12 +0200

    Refactor usage of interactable (#35)
    * refactor usage of interactable
    
    * address pr comment

--- scenes/npc/npc.gd
+++
@@ -1,7 +0,0 @@
-tool
-extends Node2D
-
-
-func _on_Interactable_interacted(_player: Player):
-	# TODO display dialog or whatever the npc should do
-	pass
--- scenes/room_switcher/room_switcher.gd
+++ scenes/room_switcher/room_switcher.gd
@@ -5,6 +5,17 @@ const Interactable := preload("res://scenes/interactable/interactable.gd")
 
 export(PackedScene) var scene_to_load
 
+onready var _interactable := get_parent().get_node(@"Interactable")
+
+
+func _ready():
+	assert(_interactable != null, "No Interactable Node")
+
+	if _interactable.connect("interacted", self, "_on_Interactable_interacted") != OK:
+		push_error(
+			str("node ", get_path(), " couldn't connect to Interactable's interacted signal")
+		)
+
 
 func _on_Interactable_interacted(_player: Player):
 	if scene_to_load != null:
